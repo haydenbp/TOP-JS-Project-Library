081d778bef27de8778f5a678509967fd4e713846
haydenbp <32523980+haydenbp@users.noreply.github.com>
2021-02-26 17:42:38 -0500

    getting things going

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
 let myLibary = [];
 
+const body = document.querySelector('#booklist');
+
+const popUp = document.querySelector('#popup')
+
+const newBookButton = document.querySelector('#addBook')
+
+const submitButton = document.querySelector('#Submit')
+
+
+
+newBookButton.addEventListener('click', () =>{
+
+  popUp.style.display = 'block'}
+
+)
+
+
+
 class Book{
 
-    constructor(name,author,pages){
+    constructor(name,author,pages,read,notes){
 
-      this.title = name
+      this['Title'] = name
       
-      this.author = author
+      this['Author'] = author
 
-      this.pages = pages
+      this['Pages'] = pages
 
+      this['Read'] = read
+
+      this['Notes'] = notes
 
     }
 
 
-
-
 }
 
-function addBookToLibrary(name,author,pages){
+function addBookToLibrary(name,author,pages,read,notes){
 
-    myLibary.push(new Book(name,author,pages))
+    myLibary.push(new Book(name,author,pages,read,notes))
 
 }
-
-addBookToLibrary('Hobbit','tolken',101)
-addBookToLibrary('LOTR','tolken',305)
+addBookToLibrary('Hobbit','tolken',101,'yes','good')
+addBookToLibrary('LOTR','tolken',305,'yes','good')
 
 console.log(myLibary)
 
-const body = document.querySelector('#booklist');
+
 
 console.log(body)
 
 for(let i = 0; i < myLibary.length; i++ ){
 
   let bookDiv = document.createElement('div')
+
+  let button = document.createElement('button')
+
+  button.innerText = 'Delete'
 
   for(let key in myLibary[i]){
 
@@ -47,4 +69,52 @@ for(let i = 0; i < myLibary.length; i++ ){
   }
 
   body.append(bookDiv)
+
+  bookDiv.appendChild(button)
 }
+
+
+submitButton.addEventListener('click', () =>{
+  
+const formAuthor = document.querySelector('#author').value
+
+const formTitle = document.querySelector('#title').value
+
+const formPages = document.querySelector('#pages').value
+
+const formRead = document.querySelector('#read').value
+
+const formNotes = document.querySelector('#notes').value
+
+let bookDiv = document.createElement('div')
+
+popUp.style.display = 'none'
+
+
+
+addBookToLibrary(formTitle,formAuthor,formPages,formRead,formNotes)
+
+
+console.log('reeee',myLibary[myLibary.length-1])
+    
+
+  for(let key in myLibary[myLibary.length-1]){
+
+  bookDiv.innerText += `${key}: ${myLibary[myLibary.length-1][key]} ` + '\n'
+    
+
+  }
+
+  document.querySelectorAll('input').forEach(element => {
+
+    element.value = ''
+    
+  });
+
+  body.append(bookDiv)
+
+  
+}
+
+
+)
